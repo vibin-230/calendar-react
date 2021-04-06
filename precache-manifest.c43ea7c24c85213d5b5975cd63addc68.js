4631998f4534130371b7b34234321c44fa1bd3c9
vibi-230 <vibin@turftown.in>
2021-04-06 18:40:41 +0530

    Updates

---
+++ precache-manifest.c43ea7c24c85213d5b5975cd63addc68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "725639dcea4e5b2c4dfead0c427b019a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "816897ae2152fc834e88",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "298a2b0aa948042adc42",
+    "url": "./static/js/2.1127deae.chunk.js"
+  },
+  {
+    "revision": "c53f65871ab74006a0c4e0b92b224267",
+    "url": "./static/js/2.1127deae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "816897ae2152fc834e88",
+    "url": "./static/js/main.58df55c1.chunk.js"
+  },
+  {
+    "revision": "56114f465e972684169c",
+    "url": "./static/js/runtime-main.92dbb504.js"
+  }
+]);
